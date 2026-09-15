6731cfbcac2eb2f8cd4140431bfa773dc121ac93
Naufal😎 <naufalsayyid895@gmail.com>
2026-09-15 10:47:16 +0700

    Revert "Update OrderController, ProductController, Product model, and API routes"

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,31 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'category_id',
-        'name',
-        'price',
-        'spicy_level',
-        'tag',
-        'description',
-        'image_url',
-        'stock',
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class, 'category_id');
-    }
-
-    public function orderItems()
-    {
-        return $this->hasMany(OrderItem::class);
-    }
+    //
 }
